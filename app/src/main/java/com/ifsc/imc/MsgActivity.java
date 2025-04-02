e8d2c0bcfaaf39de66d31982de7bc078f0fc5861
Gustavo <gustavotsalgado@gmail.com>
2025-04-01 21:38:22 -0300

    commit final

--- app/src/main/java/com/ifsc/imc/MsgActivity.java
+++ app/src/main/java/com/ifsc/imc/MsgActivity.java
@@ -2,6 +2,7 @@ package com.ifsc.imc;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MsgActivity extends AppCompatActivity {
     EditText edPeso, edAltura;
     TextView tvIMC;
+    ImageView imgResult;
+    Float imc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +24,24 @@ public class MsgActivity extends AppCompatActivity {
         edPeso = findViewById(R.id.edPeso);
         edAltura = findViewById(R.id.edAltura);
         tvIMC = findViewById(R.id.tvIMC);
+        imgResult = findViewById(R.id.imgResult);
 
         Bundle bundle = getIntent().getExtras();
+        imc = Float.valueOf(bundle.getString("IMC"));
+
+        if (imc < 18.5){
+            imgResult.setImageResource(R.drawable.abaixopeso);
+        } else if (imc >= 18.6 && imc <= 24.9) {
+            imgResult.setImageResource(R.drawable.normal);
+        } else if (imc >= 25 && imc <= 29.9) {
+            imgResult.setImageResource(R.drawable.sobrepeso);
+        } else if (imc >= 30 && imc <= 34.9) {
+            imgResult.setImageResource(R.drawable.obesidade1);
+        } else if (imc >= 35 && imc <= 39.9) {
+            imgResult.setImageResource(R.drawable.obesidade2);
+        } else
+            imgResult.setImageResource(R.drawable.obesidade3);
+
         edPeso.setText(bundle.getString("Peso") + "Kg");
         edAltura.setText(bundle.getString("Altura") + "M");
         tvIMC.setText(bundle.getString("IMC"));
