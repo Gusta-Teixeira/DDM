5bdbbfb02f3c27fa64ded050e6ef73207459f6fb
unknown <gustavotsalgado@gmail.com>
2025-04-10 22:18:44 -0300

    Primeiro commit paint

--- app/src/main/java/com/ifsc/imc/MainActivity.java
+++ app/src/main/java/com/ifsc/imc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ifsc.imc;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +16,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.ColorPickerView;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
+
 public class MainActivity extends AppCompatActivity {
+    Button btLimpa, btCor;
+    SimplePaint simplePaint;
+    ColorPickerView cpCor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btLimpa=findViewById(R.id.btLimpa);
+        simplePaint=findViewById(R.id.simplePaint);
+        btCor=findViewById(R.id.btCor);
+
+        btCor.setOnClickListener(v->{
+            new ColorPickerDialog.Builder(this)
+                    .setTitle("ColorPicker Dialog")
+                    .setPreferenceName("Paleta de cores")
+                    .setPositiveButton("Confirma",
+                            new ColorEnvelopeListener() {
+                                @Override
+                                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                                    v.setBackgroundColor(envelope.getColor());
+                                    simplePaint.mudaCor(envelope.getColor());
+                                }
+                            })
+                    .setNegativeButton("Cancela",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                    .attachAlphaSlideBar(true) // the default value is true.
+                    .attachBrightnessSlideBar(true)  // the default value is true.
+                    .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
+                    .show();
+        });
+
+        btLimpa.setOnClickListener(v->{
+            simplePaint.limpa();
+        });
 
     }
 }
