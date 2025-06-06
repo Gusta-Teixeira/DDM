ffd7f141ea30f5fbde913266480b0303f9a066d1
gustavo <gustavotsalgado@gmail.com>
2025-06-05 21:20:12 -0300

    Exemplo sql

--- app/src/main/java/com/ifsc/imc/MainActivity.java
+++ app/src/main/java/com/ifsc/imc/MainActivity.java
@@ -1,10 +1,13 @@
 package com.ifsc.imc;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,32 +25,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    PackageManager pm;
-
+    SQLiteDatabase db;
+    Button btInsere;
+    EditText edTexto;
+    ListView lvLista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        pm = getPackageManager();
+        btInsere = findViewById(R.id.button);
+        edTexto = findViewById(R.id.edTexto);
+        lvLista = findViewById(R.id.lvLista);
 
-        List<ApplicationInfo> appInfos = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        db = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas(bdID INTEGER PRIMARY KEY AUTOINCREMENT, bdTEXTO TEXT)");
 
-        for (ApplicationInfo appInfo: appInfos) {
-            Log.d("app", appInfo.packageName);
+        //db.execSQL("DELETE FROM notas");
 
+        btInsere.setOnClickListener(view -> {
+            insereNota(edTexto.getText().toString().trim());
+        });
+        carregaLv();
+
+    }
+
+    public String insereNota(String txt){
+        ContentValues cv = new ContentValues();
+        cv.put("bdTEXTO", txt);
+        db.insert("notas", null, cv);
+        carregaLv();
+        return "Inserido";
+    }
+
+    public void carregaLv(){
+            Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+            cursor.moveToFirst();
+        ArrayList<String> notas = new ArrayList<String>();
+        while (!cursor.isAfterLast()){
+            int column = cursor.getColumnIndex("bdTEXTO");
+            notas.add(cursor.getString(column));
+            cursor.moveToNext();
         }
-
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> appList = pm.queryIntentActivities(mainIntent, 0);
-
-        for (ResolveInfo resolveInfo: appList){
-            resolveInfo.loadLabel(pm).toString();
-        }
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                notas);
+        lvLista.setAdapter(adapter);
     }
 }
